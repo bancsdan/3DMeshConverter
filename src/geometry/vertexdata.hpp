d39d63b50c0ef2c0b24bc6d00902210fd7385770
Daniel Bancsi <daniel.bancsi@gmail.com>
2021-01-13 22:36:07 +0100

    Inclass initialize vertexdata to zero.

--- src/geometry/vertexdata.hpp
+++ src/geometry/vertexdata.hpp
@@ -5,9 +5,9 @@
 #include <Eigen/Dense>
 
 struct VertexData {
-    Eigen::Vector4d m_pos;
-    Eigen::Vector4d m_normal;
-    Eigen::Vector4d m_texture;
+    Eigen::Vector4d m_pos{0.0, 0.0, 0.0, 0.0};
+    Eigen::Vector4d m_normal{0.0, 0.0, 0.0, 0.0};
+    Eigen::Vector4d m_texture{0.0, 0.0, 0.0, 0.0};
 
     VertexData() = default;
     VertexData(const Eigen::Vector4d& pos) : m_pos(pos) {}
